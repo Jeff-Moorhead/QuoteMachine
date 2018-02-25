ccfd8836f9d1e976f5bd30e7a19d73fd0f5728c8
Jeff-Moorhead <Jeff.Moorhead1@gmail.com>
2018-02-24 23:17:34 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,41 @@
+var quote;
+var movie;
+
+function getRandomQuote(json) {
+    var index = Math.floor(Math.random() * json.quotes.length);
+    console.log(index);
+    quote = json.quotes[index].quote;
+    movie = json.quotes[index].movie;
+    console.log(quote);
+    linkQuote = quote.split(" ").join("+");
+    linkMovie = movie.split(" ").join("");
+    console.log(linkQuote, linkMovie);
+    $("#tweet-quote").attr("disabled", false);
+    $("#tweet-link").attr("href", "https://twitter.com/intent/tweet?text=\"" +
+        linkQuote + "\"%20%20%20&hashtags=" + linkMovie + ",freeCodeCamp" +
+        ",FrontEndWebDev");
+}
+
+function getQuote() {
+    $.getJSON("quotes.json", getRandomQuote);
+    $(".quote").fadeOut(1000, function () {
+        $(".quote").html("\"" + quote + "\"");
+        $(this).fadeIn(1000);
+    });
+
+    $(".movie").fadeOut(1000, function () {
+        $(".movie").html("- " + movie);
+        $(this).fadeIn(1000);
+    });
+
+    $(".btn-basic").fadeOut(1000, function () {
+        $(this).fadeIn(1000);
+    })
+    console.log(quote, movie);
+}
+
+$(document).ready(function () {
+    $("body").hide();
+    $("body").fadeIn(1000);
+    $("#get-quote").on("click", getQuote);
+});
